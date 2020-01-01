8d280a7cb87335f5a4ed5867d1aa3f38f049ca71
chauhansumeet <sumeetchauhan@outlook.com>
2020-01-01 16:03:50 +0530

    Initital question

---
+++ src/com/java/singtel/app/Solution.java
@@ -0,0 +1,11 @@
+package com.java.singtel.app;
+
+import com.java.singtel.model.Bird;
+
+public class Solution {
+	public static void main(String[] args) {
+		Bird bird = new Bird();
+		bird.walk();
+		bird.fly();
+	}
+}
---
+++ src/com/java/singtel/model/Animal.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.java.singtel.model;
+
+/**
+ * @author sumeet
+ *
+ */
+public class Animal {
+	public void walk() {
+		System.out.println("I am walking");
+	}
+}
